dc91e2f1fbb75c617945e6aafb1355d14da12748
R-H-Perez <R-H-Perez@users.noreply.github.com>
2021-10-26 07:26:47 +0000

    deploy: 52ecafa0372d263cdfc7cf29e08918a39d415956

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+// References the button and navbar list, in the html code.
+var button = document.querySelector("button");
+var ul = document.querySelector("ul");
 
+// The selections should come after one hits the hamburger menu, and makes it "active."
+function toggle() {
+  ul.classList.toggle("active");
+}
+
+// The event is wired so if the user clicks on the hamburger button, the selections will follow.
+button.addEventListener("click", toggle);
